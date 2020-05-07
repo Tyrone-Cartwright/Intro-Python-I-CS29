01fea916a24e7b33efac79c62c96c718bc8bce3f
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2020-05-07 07:05:51 -0400

    finished args, printing, scopes and tuples

--- src/args.py
+++ src/args.py
@@ -6,12 +6,19 @@
 
 # YOUR CODE HERE
 
-print(f1(1, 2))
+def f1(a, b):
+    return a + b
+
+print("f1 =", f1(1, 2))
+print()
 
 # Write a function f2 that takes any number of integer arguments and prints the
 # sum. Google for "python arbitrary arguments" and look for "*args"
 
 # YOUR CODE HERE
+
+def f2(*argv):
+    return sum(argv)
 
 print(f2(1))                    # Should print 1
 print(f2(1, 3))                 # Should print 4
@@ -21,13 +28,17 @@ print(f2(7, 9, 1, 3, 4, 9, 0))  # Should print 33
 a = [7, 6, 5, 4]
 
 # What thing do you have to add to make this work?
-print(f2(a))    # Should print 22
+print("f2 =", f2(*a))
+print()    # Should print 22
 
 # Write a function f3 that accepts either one or two arguments. If one argument,
 # it returns that value plus 1. If two arguments, it returns the sum of the
 # arguments. Google "python default arguments" for a hint.
 
 # YOUR CODE HERE
+
+def f3(a, b=1):
+    return a + b
 
 print(f3(1, 2))  # Should print 3
 print(f3(8))     # Should print 9
@@ -42,17 +53,22 @@ print(f3(8))     # Should print 9
 # Google "python keyword arguments".
 
 # YOUR CODE HERE
+def f4(**kwargs):
+    for key, value in kwargs.items():
+        print("key: %s, value: %s" % (key, value))
 
 # Should print
 # key: a, value: 12
 # key: b, value: 30
 f4(a=12, b=30)
+print()
 
 # Should print
 # key: city, value: Berkeley
 # key: population, value: 121240
 # key: founded, value: "March 23, 1868"
 f4(city="Berkeley", population=121240, founded="March 23, 1868")
+print()
 
 d = {
     "monster": "goblin",
@@ -60,4 +76,4 @@ d = {
 }
 
 # What thing do you have to add to make this work?
-f4(d)
+f4(**d)
