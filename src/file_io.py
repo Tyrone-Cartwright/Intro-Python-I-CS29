ff994e6685b04008ec07b8ced492f5f43e7cb56e
Tyrone Cartwright <tyrone.cartwright@yahoo.com>
2020-05-07 08:26:28 -0400

    finished remaining exercises

--- src/file_io.py
+++ src/file_io.py
@@ -4,11 +4,16 @@ at how to read and write to files here:
 
 https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 """
+import os
 
 # Open up the "foo.txt" file (which already exists) for reading
 # Print all the contents of the file, then close the file
 
 # YOUR CODE HERE
+with open('foo.txt') as f:
+    dataText = f.read()
+    print(dataText)
+f.closed
 
 # Open up a file called "bar.txt" (which doesn't exist yet) for
 # writing. Write three lines of arbitrary content to that file,
@@ -16,3 +21,8 @@ https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
 # sure that it contains what you expect it to contain
 
 # YOUR CODE HERE
+with open('bar.txt', 'w') as f:
+    string = f.write('This is my first line\nThis is my second line\nThis is my third line')
+    print(string)
+f.closed
+f = open('bar.txt')
--- src/functions.py
+++ src/functions.py
@@ -1,6 +1,8 @@
 # Write a function is_even that will return true if the passed-in number is even.
 
 # YOUR CODE HERE
+def is_even(num):
+    return num % 2 == 0
 
 # Read a number from the keyboard
 num = input("Enter a number: ")
@@ -9,4 +11,11 @@ num = int(num)
 # Print out "Even!" if the number is even. Otherwise print "Odd"
 
 # YOUR CODE HERE
+def even_odd(num):
+    if is_even(num):
+      return print("Even!")
+    else:
+        return print("Odd!")
+
+even_odd(num)
 
